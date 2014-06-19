a609f9685c628f445fc9617f337d2ed61a6c211a
Conor Branagan <conor.branagan@gmail.com>
2014-06-19 17:05:29 +0000

    Bump version of kafka-python to match requirements.txt

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class Tox(Command):
 
 setup(
     name="kafka-python",
-    version="0.9.0_1_a798c",
+    version="0.9.0_1_2fab0",
 
     install_requires=["distribute"],
     tests_require=["tox"],
